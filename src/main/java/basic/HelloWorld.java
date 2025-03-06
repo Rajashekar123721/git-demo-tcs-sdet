7cf0303b5bd6283fc6c79b973ab470d75c9b1ba1
Rajashekar123721 <g.rajashekarreddy123@gmail.com>
2025-03-06 12:12:11 +0530

    added b1 in m2

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -6,12 +6,14 @@ public class HelloWorld {
 		System.out.println("hello there!!!");
 	}
 	
-	//from b1 branch
+	//from a1 branch
 	public void m1() {
 		System.out.println("from m1");
 	}
 
+	//from b1 branch
 	public void m2() {
 		System.out.println("from m2");
+		System.out.println("from b1");
 	}
 }
